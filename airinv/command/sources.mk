778b2de2467e957ed40d6f724536c0b4ee3e8d2a
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-03-04 01:38:46 +0100

    [Dev] Began to integrate a DCP rule parser. There is still a lot of work to do.

--- airinv/command/sources.mk
+++ airinv/command/sources.mk
@@ -5,9 +5,9 @@ airinv_cmd_h_sources = \
 	$(top_srcdir)/airinv/command/InventoryParserHelper.hpp \
 	$(top_srcdir)/airinv/command/InventoryParser.hpp \
 	$(top_srcdir)/airinv/command/ScheduleParserHelper.hpp \
-	$(top_srcdir)/airinv/command/ScheduleParser.hpp \
-	$(top_srcdir)/airinv/command/DCPEventGenerator.hpp
-#  \
+	$(top_srcdir)/airinv/command/ScheduleParser.hpp
+# \
+#	$(top_srcdir)/airinv/command/DCPEventGenerator.hpp \
 #	$(top_srcdir)/airinv/command/DCPParserHelper.hpp \
 #	$(top_srcdir)/airinv/command/DCPParser.hpp
 airinv_cmd_cc_sources = \
@@ -17,8 +17,8 @@ airinv_cmd_cc_sources = \
 	$(top_srcdir)/airinv/command/InventoryParserHelper.cpp \
 	$(top_srcdir)/airinv/command/InventoryParser.cpp \
 	$(top_srcdir)/airinv/command/ScheduleParserHelper.cpp \
-	$(top_srcdir)/airinv/command/ScheduleParser.cpp \
-	$(top_srcdir)/airinv/command/DCPEventGenerator.cpp
-#  \
+	$(top_srcdir)/airinv/command/ScheduleParser.cpp
+# \
+#	$(top_srcdir)/airinv/command/DCPEventGenerator.cpp \
 #	$(top_srcdir)/airinv/command/DCPParserHelper.cpp \
 #	$(top_srcdir)/airinv/command/DCPParser.cpp
