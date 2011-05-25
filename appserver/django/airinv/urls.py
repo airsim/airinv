8e8dcaf29114c52ffeb84688a2f0a6a0661d78af
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-05-25 20:58:13 +0200

    [AppServer] Improved the Django AirInv configuration and handlers.

--- appserver/django/airinv/urls.py
+++ appserver/django/airinv/urls.py
@@ -4,12 +4,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Map ^airinv to airinv/api/urls.py (airinv.api.urls module)
-    (r'^sim/airinv/api/', include('airinv.api.urls')),
+    (r'^sim/airinv/api/', include ('airinv.api.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    (r'^sim/airinv/admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^sim/airinv/admin/doc/', include ('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^sim/airinv/admin/', include('admin.site.urls')),
+    (r'^sim/airinv/admin/', include (admin.site.urls)),
 )
 
