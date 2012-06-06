fd52e4e7ae38dee4c12c5c7c579a0c526d03bfbf
Gabrielle Sabatier <gabrielle.sabatier@amadeus.com>
2012-06-06 17:56:43 +0200

    [Dev] Added the building of default airline feature objects within the inventory generator and the inventory builder.

--- airinv/command/InventoryBuilder.cpp
+++ airinv/command/InventoryBuilder.cpp
@@ -12,6 +12,7 @@
 #include <stdair/bom/BomManager.hpp>
 #include <stdair/bom/BomRoot.hpp>
 #include <stdair/bom/Inventory.hpp>
+#include <stdair/bom/AirlineFeature.hpp>
 #include <stdair/bom/FlightDate.hpp>
 #include <stdair/bom/SegmentDate.hpp>
 #include <stdair/bom/SegmentCabin.hpp>
@@ -45,6 +46,12 @@ namespace AIRINV {
         &stdair::FacBom<stdair::Inventory>::instance().create (lKey);
       stdair::FacBomManager::addToListAndMap (ioBomRoot, *lInventory_ptr);
       stdair::FacBomManager::linkWithParent (ioBomRoot, *lInventory_ptr);
+
+      // Add the airline feature object to the inventory
+      const stdair::AirlineFeatureKey lAirlineFeatureKey (lAirlineCode);
+      stdair::AirlineFeature& lAirlineFeature =
+        stdair::FacBom<stdair::AirlineFeature>::instance().create (lAirlineFeatureKey);
+      lInventory_ptr->setAirlineFeature (lAirlineFeature);
     }
     assert (lInventory_ptr != NULL);
 
